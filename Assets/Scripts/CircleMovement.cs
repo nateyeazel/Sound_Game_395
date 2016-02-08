b75023bed8f3fcc30e088731478a74643c4356f9
Rene Romo <rromo12@gmail.com>
2016-02-07 22:20:59 -0600

    Added Enemy
    Added enemy and circular movement control script

---
+++ Assets/Scripts/CircleMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleMovement : MonoBehaviour {
+    float angle = 0;
+    float speed;
+    float x, z;
+    public float totaltime, radius;
+    //public Vector2 center;
+
+	// Use this for initialization
+	void Start () {
+        this.speed = (2 * Mathf.PI) / totaltime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        angle += this.speed * Time.deltaTime;
+        x = Mathf.Cos(angle) * radius;
+        z = Mathf.Sin(angle) * radius;
+        transform.position = (new Vector3(x, transform.position.y, z));
+    }
+}
