a445e127328394587f937b13e33702c7f1a6fb44
nateyeazel <nateyeazel@gmail.com>
2016-02-10 16:24:07 -0600

    Adding in new crazzzzy level

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,13 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public Text winText;
+	public Text countText;
+	public Text countdownText;
 
 	private Rigidbody rb;
 	private int count;
 	private bool gameOver;
+	private bool visibleObjects;
 
 	public Vector3 JumpVelocity;
 
@@ -19,21 +22,39 @@ public class PlayerController : MonoBehaviour {
 	{
 		rb = GetComponent<Rigidbody>();
 		count = 0;
-//		SetCountText ();
-//		winText.text = "";
+		SetCountText ();
+		winText.text = "";
 		gameOver = false;
+		visibleObjects = true;
 	}
 
 	void Update () {
+		
+		countdownText.text = string.Format("Time until blindness: {0}", Mathf.Max(30 - Time.realtimeSinceStartup, 0));
+
+		if(Time.realtimeSinceStartup >= 30){
+			GameObject target = GameObject.FindWithTag("Moving Target");
+			if(visibleObjects){
+				visibleObjects = false;
+				target.GetComponent<Renderer>().enabled = false;
+				target.transform.localScale += new Vector3(2.0f, 2.0f, 2.0f);
+			} else if((int)Time.realtimeSinceStartup % 5 == 0){
+				target.GetComponent<Renderer>().enabled = true;
+			} else if((int)Time.realtimeSinceStartup % 5 == 1){
+				target.GetComponent<Renderer>().enabled = false;
+			}
+		} 
 
 		if(Input.GetButtonDown("Jump")) {
 			this.GetComponent<Rigidbody>().AddForce(JumpVelocity, ForceMode.VelocityChange);
 		}
+
 		if(rb.position.y <= -10) {
 			gameOver = true;
             Time.timeScale = 0;
             winText.text = "Loser, hit R to restart";
 		}
+
 		if(gameOver && Input.GetKeyDown("r")){
 			SceneManager.LoadScene(SceneManager.GetActiveScene ().name);
             Time.timeScale = 1;
@@ -56,7 +77,7 @@ public class PlayerController : MonoBehaviour {
 		{
 			other.gameObject.SetActive (false);
 			count = count + 1;
-//			SetCountText ();
+			SetCountText ();
 		}
 
         if (other.gameObject.CompareTag("Enemy"))
@@ -71,14 +92,22 @@ public class PlayerController : MonoBehaviour {
             }
 
         }
+		if (other.gameObject.CompareTag ("Moving Target"))
+		{
+			float new_x = Random.Range(-10f, 10f);
+			float new_z = Random.Range(-10f, 10f);
+			other.gameObject.transform.position = new Vector3(new_x, 0.5f, new_z);
+			count = count + 1;
+			SetCountText ();
+		}
     }
 
-//	void SetCountText ()
-//	{
-//		countText.text = "Count: " + count.ToString ();
-//		if (count >= 7)
-//		{
-//			winText.text = "You Win!";
-//		}
-//	}
+	void SetCountText ()
+	{
+		countText.text = "Collected: " + count.ToString ();
+		if (count >= 15)
+		{
+			winText.text = "You Win!";
+		}
+	}
 }
